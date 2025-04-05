4f8b636a89e00e74265369e1e95956ffe3f8747d
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-04-05 22:04:51 +0500

    Building bttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <button className=" bg-green-600">Enter</button>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default HomePage;
